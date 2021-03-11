34100037c3948de179ce383fd668b4c553220349
Syawadhilah Pradipta <dipta0988@gmail.com>
2021-03-11 02:21:58 -0500

    update login button

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ var express = require('express');       //shows that we use the express
 var app = express();
 
 var server = app.listen(5000, listening);
+// var login = require('./public/login');
 
 function listening(){
     console.log("listening...");
 }
 
 app.use(express.static('public'));
-
+// app.use('/login', login);
 
 console.log('server is Starting');
